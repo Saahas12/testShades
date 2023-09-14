a1cae324881c955b04eb151049c2c95070068477
Saahas12 <saahaspatkar@gmail.com>
2023-09-14 14:37:48 +0530

    added all files from slave branch

---
+++ script2.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const productCards = document.querySelectorAll('.gallery');
+    const locationDropdown = document.getElementById('locationDropdown');
+  
+    fetch('products1.json')
+      .then(response => response.json())
+      .then(data => {
+          console.log(data)
+        
+        locationDropdown.addEventListener('change', function () {
+          const selectedLocation = locationDropdown.value;
+          productCards.forEach((productCard, index) => {
+            const productInfo = data[index];
+            updateProductCard(productCard, productInfo, selectedLocation);
+          });
+        });
+  
+        // Initialize with default location
+        const defaultLocation = locationDropdown.value;
+        productCards.forEach((productCard, index) => {
+          const productInfo = data[index];
+          updateProductCard(productCard, productInfo, defaultLocation);
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  
+    function updateProductCard(card, productInfo, location) {
+        // if (!productInfo) {
+        //     console.error('productInfo is undefined');
+        //     return;
+        //   }
+      const productNameElement = card.querySelector('#productName');
+      const productCategoryElement = card.querySelector('#productCategory');
+      const productPriceElement = card.querySelector('#productPrice');
+      const productImageElement = card.querySelector('#productImage');
+  
+      productNameElement.textContent = productInfo.name;
+      productCategoryElement.textContent = productInfo.category;
+      const selectedPrice = productInfo.prices[location] || { amount: 0, currency: '', rate: 1 };
+
+      // Convert price to USD using the exchange rate
+      const convertedPriceUSD = selectedPrice.amount * parseFloat(selectedPrice.rate);
+      productPriceElement.textContent = `${selectedPrice.currency}${convertedPriceUSD.toFixed(2)}`;
+      productImageElement.src = productInfo.image;
+      productImageElement.alt = `${productInfo.name} Image`;
+    }
+  });
+  
